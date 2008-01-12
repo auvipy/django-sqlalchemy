e20143532da7e4e91ee13893966e2ac78597fe5c
brosner <brosner@4e6132d6-6443-0410-b91b-6b3f868a490e>
2008-01-12 18:42:37 +0000

    added metaclass support to ClassReplacer (not working) and tests.

--- django_sqlalchemy/utils.py
+++ django_sqlalchemy/utils.py
@@ -6,39 +6,92 @@ class ClassReplacer(object):
     """
     Acts as a metaclass to replace the given class with the metaclass class.
     An improved version originally found in the sadjango project.
+    
+    Create the base metaclass and base class.
+    
+    >>> class BaseMetaclass(type):
+    ...     def __new__(cls, name, bases, attrs):
+    ...         print "BaseMetaclass.__new__ called"
+    ...         return super(BaseMetaclass, cls).__new__(cls, name, bases, attrs)
+
+    >>> class BaseClass(object):
+    ...     __metaclass__ = BaseMetaclass
+    ...
+    ...     def __init__(self):
+    ...         print "BaseClass.__init__ called"
+    ...
+    ...     def method1(self):
+    ...         print "BaseClass.method1 called"
+    BaseMetaclass.__new__ called
+    
+    >>> class MyMetaclass(type):
+    ...     def __new__(cls, name, bases, attrs):
+    ...         print "MyMetaclass.__new__ called"
+    ...         return super(MyMetaclass, cls).__new__(cls, name, bases, attrs)
+    
+    >>> class MyClass(object):
+    ...     __metaclass__ = ClassReplacer(BaseClass, MyMetaclass)
+    ...
+    ...     def __init__(self):
+    ...         print "MyClass.__init__ called"
+    ...         self._original.__init__(self)
+    ...
+    ...     def method2(self):
+    ...         print "MyClass.method2 called"
+    
+    >>> BaseClass.__metaclass__
+    <class '__main__.MyMetaclass'>
+    
+    >>> class SubClass(BaseClass):
+    ...     def __init__(self):
+    ...         super(SubClass, self).__init__()
+    ...         print "SubClass.__init__ called"
+    ...
+    ...     def method3(self):
+    ...         print "SubClass.method3 called"
+    MyMetaclass.__new__ called
+    
+    >>> instance = SubClass()
+    MyClass.__init__ called
+    BaseClass.__init__ called
+    SubClass.__init__ called
+    
+    >>> instance.method3()
+    SubClass.method3 called
+    
+    >>> myclass_instance = MyClass()
+    MyClass.__init__ called
+    BaseClass.__init__ called
+    >>> myclass_instance.method2()
+    MyClass.method2 called
+    
+    >>> baseclass_instance = BaseClass()
+    MyClass.__init__ called
+    BaseClass.__init__ called
+    >>> baseclass_instance.method1()
+    BaseClass.method1 called
     """
-    def __init__(self, klass):
+    
+    def __init__(self, klass, metaclass=None):
         self.klass = klass
+        self.metaclass = metaclass
     
     def __call__(self, name, bases, attrs):
         for name, value in attrs.items():
-            if name in ("__class__", "__bases__", "__metaclass__", "__module__"):
+            if name in ("__class__", "__bases__", "__module__"):
                 continue
-            setattr(self.klass, "_original", MethodContainer())
-            if hasattr(self.klass, name):
-                setattr(self.klass._original, name, getattr(self.klass, name))
-            setattr(self.klass, name, value)
+            if name == "__metaclass__":
+                setattr(self.klass, "__metaclass__", self.metaclass)
+            else:
+                setattr(self.klass, "_original", MethodContainer())
+                if hasattr(self.klass, name):
+                    setattr(self.klass._original, name, getattr(self.klass, name))
+                setattr(self.klass, name, value)
         return self.klass
 
+def _test():
+    import doctest
+    doctest.testmod()
+
 if __name__ == "__main__":
-    # TODO: move tests to tests and write more tests :)
-    class Field(object):
-        def my_method(self):
-            print "original MyClass::my_method"
-    
-    class Field(object):
-        __metaclass__ = ClassReplacer(Field)
-        
-        def my_new_method(self):
-            print "new MyClass::my_new_method"
-        
-        def my_method(self):
-            print "new MyClass::my_method"
-    
-    class CharField(Field):
-        def my_method(self):
-            super(CharField, self).my_method()
-            print "charfield my_method"
-    
-    cf = CharField()
-    cf.my_method()
+    _test()
