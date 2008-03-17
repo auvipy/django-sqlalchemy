9513d31e72fec1d32b890b959209dc23dcd3cfee
akgomaa <akgomaa@4e6132d6-6443-0410-b91b-6b3f868a490e>
2008-03-17 22:10:53 +0000

    Final SVN commit, yay for DVCS.

--- tests/regression/sample_app/models.py
+++ tests/regression/sample_app/models.py
@@ -1,7 +1,13 @@
 from django_sqlalchemy import models
 
-class Publisher(models.Model):
+class Reporter(models.Model):
     username = models.CharField(max_length=32)
     
+class Source(models.Model):
+    codename = models.CharField(max_length=32, null=False, default="Deep Throat")
 
-
+class Article(models.Model):
+    sources = models.ManyToManyField(Source, related_name="stories")
+    author = models.ForeignKey(Reporter, related_name="stories")
+    content = models.TextField()
+    
