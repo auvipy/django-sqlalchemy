dedb676ea478304bc9f1c1d92021963b1c149824
akgomaa <akgomaa@4e6132d6-6443-0410-b91b-6b3f868a490e>
2008-03-17 19:33:55 +0000

    Remove the non-test content inside of tests, add a run_tests script and default settings. Still need to fix database creation before tests can run.

---
+++ trunk/tests/settings_sqlite.py
@@ -0,0 +1,21 @@
+
+DEBUG = TEMPLATE_DEBUG = True
+
+DATABASE_ENGINE = 'django_sqlalchemy'
+DATABASE_NAME = ''
+DJANGO_SQLALCHEMY_DBURI = "sqlite:///testing.sqlite"
+DJANGO_SQLALCHEMY_ECHO = True
+
+INSTALLED_APPS = (
+    'django_sqlalchemy',
+    )
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
+)
+
+# Of note here will be the TransactionMiddleware. 
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+)
