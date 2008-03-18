d370565c84af38f0492476ddfe37ba5d73537de3
mtrier <mtrier@4e6132d6-6443-0410-b91b-6b3f868a490e>
2008-03-18 07:07:21 +0000

    Added in the ForeignKey related class.  Not complete.

--- django_sqlalchemy/models/__init__.py
+++ django_sqlalchemy/models/__init__.py
@@ -2,6 +2,7 @@ import sqlalchemy
 from sqlalchemy import *
 from sqlalchemy.types import *
 from django_sqlalchemy.models.fields import *
+from django_sqlalchemy.models.fields.related import ForeignKey
 from django.conf import settings
 from django_sqlalchemy.models.base import Model
 
--- sample/foo/models.py
+++ sample/foo/models.py
@@ -4,10 +4,16 @@ from django_sqlalchemy import models
 # Create your models here.
 class Category(models.Model):
     """Category Class"""
-    __tablename__ = 'foo_category'
-    
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(default=datetime.datetime.now)
     
     def __unicode__(self):
         return self.name
+    
+class Post(models.Model):
+    pub_date = models.DateTimeField(default=datetime.datetime.now)
+    category = models.ForeignKey(Category)
+    body = models.TextField(blank=True)
+    
+    def __unicode__(self):
+        return self.body
