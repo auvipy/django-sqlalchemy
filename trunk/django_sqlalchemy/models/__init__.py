83038164736ed9293caa2ea3ffab19149773bd78
mtrier <mtrier@4e6132d6-6443-0410-b91b-6b3f868a490e>
2008-03-18 04:32:54 +0000

    cleaned up some properties in the fields that don't need to be there. Cleared out the related file and got it ready for related field mapping.

--- trunk/django_sqlalchemy/models/__init__.py
+++ trunk/django_sqlalchemy/models/__init__.py
@@ -5,12 +5,7 @@ from django_sqlalchemy.models.fields import *
 from django.conf import settings
 from django_sqlalchemy.models.base import Model
 
-__all__ = ['Field', 'AutoField', 'CharField', 'PhoneNumberField',
-           'metadata', 'session'] + \
+__all__ = ['Field', 'AutoField', 'CharField', 'PhoneNumberField'] + \
            sqlalchemy.types.__all__
-
-__doc_all__ = ['create_all', 'drop_all',
-	           'setup_all', 'cleanup_all',
-	           'metadata', 'session']
 
 
