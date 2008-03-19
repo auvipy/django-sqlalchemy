8eb406c45f55356f7061c2c29ec1b10a8e7e5411
brosner <brosner@4e6132d6-6443-0410-b91b-6b3f868a490e>
2008-03-19 00:02:11 +0000

    Fixed syncdb as I just broked it ;)

--- django_sqlalchemy/management/commands/syncdb.py
+++ django_sqlalchemy/management/commands/syncdb.py
@@ -1,6 +1,6 @@
 from optparse import make_option
 from django.core.management.base import NoArgsCommand
-from django_sqlalchemy.models import metadata
+from django_sqlalchemy.backend.base import metadata
 
 class Command(NoArgsCommand):
     option_list = NoArgsCommand.option_list + (
