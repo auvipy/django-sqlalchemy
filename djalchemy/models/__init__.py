975ec0149feb76ab8534174c178294fa02e52a7b
Asif Saifuddin Auvi <auvipy@gmail.com>
2016-06-13 00:36:58 +0600

    style fix and import adjustment

--- djalchemy/models/__init__.py
+++ djalchemy/models/__init__.py
@@ -1,12 +1,11 @@
 import sqlalchemy
 from sqlalchemy import *
 from sqlalchemy.types import *
-from django_sqlalchemy.models.fields import *
-from django_sqlalchemy.models.fields.related import ForeignKey, ManyToManyField
+from djalchemy.models.fields import *
+from djalchemy.models.fields.related import ForeignKey, ManyToManyField
 from django.conf import settings
 from django_sqlalchemy.models.base import Model
 
-__all__ = ['Field', 'AutoField', 'CharField', 'PhoneNumberField'] + \
-           sqlalchemy.types.__all__
-
-
+__all__ = [
+    'Field', 'AutoField', 'CharField', 'PhoneNumberField'] + \
+    sqlalchemy.types.__all__
